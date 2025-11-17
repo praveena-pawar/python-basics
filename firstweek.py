ec656c1cdc8f0e0c88fc9bc68888daecb5f87684
praveena-pawar <praveena.dev02@gmail.com>
2025-11-17 23:44:12 +0530

    even or odd program

---
+++ firstweek.py
@@ -0,0 +1,108 @@
+# print("hello world")
+
+# a = 10 ;
+# b =  20 ;
+# sum = 10 + 20;
+# print(sum);
+
+
+# total = 10
+# TOTAL = 20
+# print(type(total))
+# # print(total)
+# print(TOTAL)
+
+# import keyword
+# keyword.kwlist;
+
+
+
+# name = 'praveena'
+# print(name[0:100])
+# # print(name[-1])
+# # print(name[5])
+
+# int('10')
+# print(int)
+
+
+# x = "hyd"
+# y = "hyd"
+# print(id(x))
+# print(id(y))
+
+
+# x = [10, 20, 30, 40]
+# b=bytearray(x)
+# b [0] = 100
+# for x in b : print(x)
+# # print(x)ff
+
+
+# l = []
+# l.append(10)
+# l.append(50)
+# l.append("praveena")
+# print(l)
+
+
+# t = (10, 20,"praveena")
+# print(type(t))
+
+
+# age = int(input("Enter age :"))
+# if age >= 18 :
+#     print("you can vote...")
+# else :
+#     print("your too young....")
+
+
+# color = input("Enter color:")
+# if color == "red" :
+#     print("stop here..")
+# elif color == "green" :
+#     print("you can go..")
+# elif color == "yellow" :
+#     print("get ready..")
+# else :
+#     print("wrong color")
+
+
+# age = int(input("Enter your age :"))
+# if age < 13 :
+#     print("your child..")
+# elif (age > 13 and age < 18) :
+#     print("your an teen ager..")
+# elif age > 18 :
+#     print("your adult..")
+
+
+
+
+# user_name = input("Enter user name :")
+# password = int(input("Enter your password :"))
+# if (user_name == "praveena" and password == 2025) :
+#     print("hello praveena..")
+# elif (user_name != "praveena") :
+#     print("Wong user name")
+# elif (password != 2025):
+#     print("wrong pass word")
+# else :
+#     print("wrong password")
+
+
+
+# n = int(input("Enter number :"))
+# if n%5 == 0 :
+#     print("n is multipla of 5")
+# else :
+#     print("it is not multipl of 5")
+
+
+
+
+n = int(input("Enter a number :"))
+if (n % 2 == 0) :
+    print("number is even")
+else :
+    print("number is odd")
